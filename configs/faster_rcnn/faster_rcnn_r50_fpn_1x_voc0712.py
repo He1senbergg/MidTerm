2d2d2b2dad68825d38bee40c1d210fc7ab9be955
He1senbergg <148076707+He1senbergg@users.noreply.github.com>
2024-05-24 16:54:09 +0800

    Update faster_rcnn_r50_fpn_1x_voc0712.py

--- configs/faster_rcnn/faster_rcnn_r50_fpn_1x_voc0712.py
+++ configs/faster_rcnn/faster_rcnn_r50_fpn_1x_voc0712.py
@@ -3,3 +3,5 @@ _base_ = [
     '../_base_/datasets/voc0712.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
+
+load_from = "checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth"
