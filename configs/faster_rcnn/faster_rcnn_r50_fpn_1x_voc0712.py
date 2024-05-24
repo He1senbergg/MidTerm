c0a8f5a7ca6ea581d953ff2fa4c33fd1f4db43fd
He1senbergg <148076707+He1senbergg@users.noreply.github.com>
2024-05-24 09:54:48 +0800

    Update faster_rcnn_r50_fpn_1x_voc0712.py

--- configs/faster_rcnn/faster_rcnn_r50_fpn_1x_voc0712.py
+++ configs/faster_rcnn/faster_rcnn_r50_fpn_1x_voc0712.py
@@ -1,13 +1,5 @@
 _base_ = [
-    '../_base_/models/faster-rcnn_r50_fpn voc.py',
-    '../_base_/datasets/voc0712 copy1.py',
-    '../_base_/schedules/schedule_1x copy1.py', '../_base_/default_runtime copy1.py'
+    '../_base_/models/faster-rcnn_r50_fpn.py',
+    '../_base_/datasets/voc0712.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
-
-# 模型设置
-model = dict(
-    roi_head=dict(
-        bbox_head=dict(num_classes=20)))  # VOC数据集有20个类别
-
-# 预训练权重
-load_from = 'checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
